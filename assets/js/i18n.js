7145518cfdc7ac921da4c79185bc8bc4c69e511c
ardazeyrek0138-lab <ardazeyrek0138@gmail.com>
2025-10-03 21:29:02 +0300

    Create i18n.js

---
+++ assets/js/i18n.js
@@ -0,0 +1,16 @@
+{
+  "menu_dashboard": "Dashboard",
+  "menu_tables": "Tables",
+  "menu_settings": "Settings",
+  "menu_forms": "Forms",
+  "menu_components": "Components",
+  "header_profile": "Profile"
+}
+{
+  "menu_dashboard": "Anasayfa",
+  "menu_tables": "Tablolar",
+  "menu_settings": "Ayarlar",
+  "menu_forms": "Formlar",
+  "menu_components": "Bileşenler",
+  "header_profile": "Profil"
+}
